6ce06ce09074e8bf8eb01a7f074d722dad46bf0f
belmiro3008 <belmiro.tobias88@gmail.com>
2021-08-07 19:14:46 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/belmiro/bookstore/domain/Categoria.java
@@ -0,0 +1,56 @@
+package com.belmiro.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private List<Livro> livros = new ArrayList<Livro>();
+
+	public Categoria() {
+
+	}
+
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
