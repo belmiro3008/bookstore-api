143e17508f00293ce2499524cefc775d347d7cb4
belmiro3008 <belmiro.tobias88@gmail.com>
2021-08-07 19:31:55 -0300

    finalizando as classes de dominio

--- src/main/java/com/belmiro/bookstore/domain/Categoria.java
+++ src/main/java/com/belmiro/bookstore/domain/Categoria.java
@@ -1,13 +1,26 @@
 package com.belmiro.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-public class Categoria {
-
+@Entity
+public class Categoria implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<Livro>();
 
 	public Categoria() {
@@ -52,5 +65,29 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", livros=" + livros + "]";
+	}
+	
+	
 
 }
